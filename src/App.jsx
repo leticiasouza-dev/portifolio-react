2b45d2a93700e6abe489db414b440653e5a25b17
leticiaSouzaDev <lec667121@gmail.com>
2024-05-17 14:17:39 -0300

    feat: adicionando a estrutura e estilização da sessão com informações sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 
+import * as S from './StyleApp.jsx'
+
 import Header from './components/Header/Header'
+import fotoPerfil from "./assets/backgrounds/perfil.jpg"
 
 import './App.css'
 
 function App() {
-  
-
   return (
-    <Header/>
+    <>
+      <Header/>
+
+      <S.Main>
+
+        <S.SectionSobreMim>
+
+          <S.divContainer>
+            <h2>Olá, sou Letícia Souza, desenvolvedora de páginas web criativas e funcionais.</h2>
+
+            <p>Atualmente, estou cursando Técnico em Desenvolvimento de Sistemas e realizando um curso intensivo de Full Stack com duração de um ano, aprimorando minhas habilidades para criar soluções web completas</p>
+          </S.divContainer>
+
+          <img src={fotoPerfil} alt="foto de perfil" />
+          
+        </S.SectionSobreMim>
+        
+      </S.Main>
+    </>
+    
   )
 }
 
---
+++ src/StyleApp.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+
+export const Main = styled.main`
+    padding: 20px 6rem;
+
+    background-color: red;
+
+    @media screen and (max-width:995px) { // laptos menores
+        padding: 20px 2rem;
+    }
+
+    @media screen and (max-width: 768px){ // para tablets
+        padding: 1rem 2rem;
+    }
+`
+
+export const SectionSobreMim = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img{
+        border-radius: 50%;
+    }
+
+    @media screen and (max-width:995px) { // laptos menores
+        img{
+            height: 10rem;
+        }
+    }
+`
+
+export const divContainer = styled.div`
+    width: 70%;
+    background-color: aqua;
+
+    h2{
+        font-size: 2.5rem;
+    }
+
+    @media screen and (max-width:995px) { // laptos menores
+        h2{
+            font-size: 1.5rem;
+        }
+    }
+`
+
